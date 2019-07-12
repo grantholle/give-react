0d49baa5ec2f0ed5fec3cf514a70778a78d293ac
Grant Holle <hollegrant@gmail.com>
2019-07-12 09:40:51 +0800

    Move sign in form

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import Layout from "./components/layout"
-import Blocks from "./components/blocks"
+import Layout from "../components/layout"
+import Blocks from "../components/blocks"
 
 export default () => (
   <Layout>
